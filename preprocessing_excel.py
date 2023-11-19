aa57957dc238e09c47b0947016fb6c11f0482cf8
Dvd1198 <davidecosenza98@gmail.com>
2023-11-19 21:36:49 +0100

    first commit

---
+++ preprocessing_excel.py
@@ -0,0 +1,43 @@
+import openpyxl as xl
+import numpy as np
+
+# original pieces must be defined in the first row of matrix in excel, the script then rotates/flips them
+
+wb = xl.load_workbook('Blokus Pieces.xlsx', data_only=True) # workbook
+ws = wb['Pieces'] # worksheet
+d_cell = 11 # dimension of excel cell used
+n_pieces = 21 # number of blokus pieces (standard game)
+n_variant = 8 # max number of flip and rotation for each piece
+
+for p_id in range(n_pieces):
+    print('Piece ID: ' + str(p_id))
+    orig_mat = np.zeros((d_cell, d_cell)) # original 11 x 11 matrix
+    start_xl_col = 2 + p_id*d_cell # starting column on excel
+    start_xl_row = 2 # starting row on excel
+    
+    # read original piece
+    for i in range(d_cell): # rows
+        for j in range(d_cell): # columns
+            orig_mat[i,j] = ws.cell(row=start_xl_row+i , column=start_xl_col+j).value
+            
+    # rotate 3 times +90, +180, + 270
+    for k in [1, 2, 3]:
+        new_mat = np.rot90(orig_mat, k)
+        start_xl_row = 2 + k*d_cell
+        # write new piece
+        for i in range(d_cell): # rows
+            for j in range(d_cell): # columns
+                ws.cell(row=start_xl_row+i , column=start_xl_col+j).value = new_mat[i,j]
+                
+    # flip, then rotate 4 times 0, +90, +180, + 270
+    flip_mat = np.fliplr(orig_mat) # flip horizontally
+    for k in [0, 1, 2, 3]:
+        new_mat = np.rot90(flip_mat, k)
+        start_xl_row = 2 + (4 + k)*d_cell
+        # write new piece
+        for i in range(d_cell): # rows
+            for j in range(d_cell): # columns
+                ws.cell(row=start_xl_row+i , column=start_xl_col+j).value = new_mat[i,j]
+    
+wb.save('Blokus Pieces.xlsx')
+    
---
+++ preprocessing_id.py
@@ -0,0 +1,74 @@
+import openpyxl as xl
+import numpy as np
+import pickle as pk
+
+# must be run after preprocessing_excel.py
+# calculates indexes of pieces and attachment points
+
+
+def preprocess_id():
+    wb = xl.load_workbook('Blokus Pieces.xlsx', data_only=True) # workbook
+    ws = wb['Pieces'] # worksheet
+    d_cell = 11 # dimension of excel cell used
+    n_pieces = 21 # number of blokus pieces (standard game)
+    n_variant = 8 # max number of flip and rotation for each piece
+    # squares relative position coord: n_pieces x n_variant x 5 (max number of square for piece) x 2 (row, column)
+    # row = 0, col = 0 is the relative coord of the origin square, always defined for every block
+    position_square = np.zeros((n_pieces, n_variant, 5, 2), dtype=np.int8)
+    count_pos_squares = np.zeros((n_pieces, n_variant), dtype=np.int8) # counter of square coord, used for indexing
+
+    # attachment squares relative position coord: n_pieces x n_variant x 8 (max number of attachment squares, cross piece) x 2 (row, column)
+    position_attach = np.zeros((n_pieces, n_variant, 8, 2), dtype=np.int8)
+    count_pos_attach = np.zeros((n_pieces, n_variant), dtype=np.int8) # counter of attachment coord, used for indexing
+    
+    # forbidden squares relative position coord: n_pieces x n_variant x 12 (max number of attachment squares, long straight piece) x 2 (row, column)
+    position_forbid = np.zeros((n_pieces, n_variant, 12, 2), dtype=np.int8)
+    count_pos_forbid = np.zeros((n_pieces, n_variant), dtype=np.int8) # counter of forbidden coord, used for indexing
+
+    for p_id in range(n_pieces):
+        for v_id in range(n_variant):
+            
+            start_xl_col = 2 + p_id*d_cell # starting column on excel
+            start_xl_row = 2 + v_id*d_cell # starting row on excel
+            
+            # read cells
+            curr_mat = np.zeros((d_cell, d_cell))
+            for i in range(d_cell): # rows
+                for j in range(d_cell): # columns
+                    curr_mat[i,j] = ws.cell(row=start_xl_row+i , column=start_xl_col+j).value    
+                    # center of the matrix "curr_mat" is in [5,5] and always contains the origin of the piece
+                    rel_row = i - 5
+                    rel_col = j - 5
+                          
+                    if curr_mat[i,j] == 1 or curr_mat[i,j] == 2: # 1 = origin square of piece, 2 = other squares
+                        count = count_pos_squares[p_id, v_id]
+                        # updating the position_square array
+                        position_square[p_id, v_id, count, 0] = rel_row
+                        position_square[p_id, v_id, count, 1] = rel_col
+                        # updating counter
+                        count_pos_squares[p_id, v_id] = count + 1
+                        
+                    elif curr_mat[i,j] == 5: # 5 = attachment points
+                        count = count_pos_attach[p_id, v_id]
+                        # updating the position_attach array
+                        position_attach[p_id, v_id, count, 0] = rel_row
+                        position_attach[p_id, v_id, count, 1] = rel_col
+                        # updating counter
+                        count_pos_attach[p_id, v_id] = count + 1
+                    
+                    elif curr_mat[i,j] == 6: # 6 = forbidden squares
+                        count = count_pos_forbid[p_id, v_id]
+                        # updating the position_attach array
+                        position_forbid[p_id, v_id, count, 0] = rel_row
+                        position_forbid[p_id, v_id, count, 1] = rel_col
+                        # updating counter
+                        count_pos_forbid[p_id, v_id] = count + 1
+    
+    # saving using pickle
+    with open('pieces_data.pickle', 'wb') as handle:
+        pk.dump([position_square, count_pos_squares, position_attach, count_pos_attach, position_forbid, count_pos_forbid], handle)
+    pass
+                    
+                    
+preprocess_id()
+        
